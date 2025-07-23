ed8bbe7c15cea9a58ce21c322d6ad28e0e332496
GemmaTuron <gemma@ersilia.io>
2025-07-23 17:09:56 +0300

    upgrade model

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM bentoml/model-server:0.11.0-py310
+FROM bentoml/model-server:0.11.0-py312
 MAINTAINER ersilia
 
-RUN apt-get update && apt-get install -y libgomp1 && rm -rf /var/lib/apt/lists/*
-RUN pip install rdkit==2023.3.2
-RUN pip install git+https://github.com/ersilia-os/lazy-qsar.git@b930abc17da7b2c18be1ad48440593ca7a6edbf3
-RUN pip install scikit-learn==1.2.2
+RUN pip install lazyqsar==1.0
+RUN pip install numpy==1.26.4
+RUN pip install rdkit==2023.9.5
+RUN pip install chemprop==2.2.0
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,7 +2,7 @@
 import os
 import csv
 import sys
-import joblib
+import lazyqsar
 
 # parse arguments
 input_file = sys.argv[1]
@@ -12,17 +12,17 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 MODELPATH = os.path.join(root, "..", "..", "checkpoints")
+model_5um = os.path.join(MODELPATH, "ic50_hepg2_72h_5uM")
+model_10um = os.path.join(MODELPATH, "ic50_hepg2_72h_10uM")
 
 # my model
-def my_model(smiles_list):
-    mdl1 = joblib.load(os.path.join(MODELPATH, "ic50_hepg2_72h_5uM_eosce.joblib"))
-    mdl2 = joblib.load(os.path.join(MODELPATH, "ic50_hepg2_72h_10uM_eosce.joblib"))
-
-    y_pred1 = mdl1.predict_proba(smiles_list)[:,1]
-    y_pred2 = mdl2.predict_proba(smiles_list)[:,1]
-
-    return y_pred1, y_pred2
-
+def predict(smiles, model_dir):
+    print(f"Length of the X sample: {len(smiles)}")
+    model = lazyqsar.LazyBinaryClassifier.load_model(model_dir)
+    chemeleon = lazyqsar.descriptors.ChemeleonDescriptor()
+    X = chemeleon.transform(smiles)
+    y_hat = model.predict_proba(X=X)[:,1]
+    return y_hat
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -31,11 +31,13 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-output1, output2 = my_model(smiles_list)
+output1 = predict(smiles_list, model_5um)
+output2 = predict(smiles_list, model_10um)
+
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["HepG2_IC50_72h_5uM".lower(), "HepG2_IC50_72h_10uM".lower()])  # header with column names
+    writer.writerow(["HepG2_IC50_72h_5uM".lower(), "HepG2_IC50_72h_10uM".lower()])
     for o1, o2, in zip(output1, output2):
         writer.writerow([o1, o2])
