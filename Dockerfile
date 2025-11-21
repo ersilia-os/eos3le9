252a55223405eefa03676a46af6a79de4c84a262
gemmaturon <gemma@ersilia.io>
2025-11-21 16:45:03 +0100

    models lq v2

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM bentoml/model-server:0.11.0-py312
 MAINTAINER ersilia
 
-RUN pip install lazyqsar==1.0
-RUN pip install numpy==1.26.4
-RUN pip install rdkit==2023.9.5
-RUN pip install chemprop==2.2.0
+RUN pip install lazyqsar[descriptors]==2.1.1
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,8 +1,7 @@
-# imports
 import os
 import csv
 import sys
-import lazyqsar
+from lazyqsar.qsar import LazyBinaryQSAR
 
 # parse arguments
 input_file = sys.argv[1]
@@ -12,17 +11,8 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 MODELPATH = os.path.join(root, "..", "..", "checkpoints")
-model_5um = os.path.join(MODELPATH, "ic50_hepg2_72h_5uM")
-model_10um = os.path.join(MODELPATH, "ic50_hepg2_72h_10uM")
 
-# my model
-def predict(smiles, model_dir):
-    print(f"Length of the X sample: {len(smiles)}")
-    model = lazyqsar.LazyBinaryClassifier.load_model(model_dir)
-    chemeleon = lazyqsar.descriptors.ChemeleonDescriptor()
-    X = chemeleon.transform(smiles)
-    y_hat = model.predict_proba(X=X)[:,1]
-    return y_hat
+models = ["ic50_hepg2_72h_5um", "ic50_hepg2_72h_10um"]
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -31,13 +21,18 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-output1 = predict(smiles_list, model_5um)
-output2 = predict(smiles_list, model_10um)
+y_preds = {}
+for m in models:
+    model_folder = os.path.join(MODELPATH, f"{m}")
+    model = LazyBinaryQSAR.load(model_folder)
+    y_pred = model.predict_proba(smiles_list=smiles_list)[:, 1]
+    y_preds[m]=y_pred
 
+header = list(y_preds.keys())
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["HepG2_IC50_72h_5uM".lower(), "HepG2_IC50_72h_10uM".lower()])
-    for o1, o2, in zip(output1, output2):
+    writer.writerow(["ic50_hepg2_72h_5um", "ic50_hepg2_72h_10um"]) 
+    for o1, o2 in zip(*(y_preds[m].tolist() for m in header)):
         writer.writerow([o1, o2])
